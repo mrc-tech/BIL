e0ef210116d9ee6133ec0e94db0a0dedb5b9b644
mrc-tech <diescc@gmail.com>
2021-06-25 00:47:03 +0200

    writeBMP
    funzione che scrive un array di interi da 0 a 255 in un'immagine BMP secondo la scala di grigio corrispondente

---
+++ examples/writeBMP.cpp
@@ -0,0 +1,29 @@
+/**************************************
+writes a bitmap file with all the shades of gray
+possible using 24-bit per pixel bitmp file
+**************************************/
+
+#include "../include/writeBMP.h"
+
+using namespace std;
+
+
+
+int main()
+{
+	int width  = 256;
+	int height = 256;
+	int data[width*height];
+	
+	int count = 0;
+	for(int i=0;i<height;i++){
+		for(int j=0;j<width;j++){
+			data[i*width + j] = count++;
+			if(count>255) count = 0;
+		}
+	}
+	
+	writeBMP("writeBMP_test.bmp", width,height, data);
+	
+	return 0;
+}
