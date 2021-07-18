44d598bed48cdbf968781965773bf9fbb9c8622c
mrc-tech <74192712+mrc-tech@users.noreply.github.com>
2021-07-19 01:12:40 +0200

    Update interp1.h
    aggiunto il `typename` anche alla seconda funzione. Non sono comunque sicuro di usare il typename per queste funzioni (che lavorano meglio in double)....

--- include/utils/interp1.h
+++ include/utils/interp1.h
@@ -21,7 +21,7 @@ double interp1(std::vector<T> x, std::vector<T> y, T val)
 		return 0;
 	}
 	
-	int i=0; //indice della x minore (x_(i+1) è quella maggiore)
+	int i=0; //indice della x minore (x_(i+1) Ã¨ quella maggiore)
 	//cerca il range delle x in cui ricade "val":
 	while(x[i+1] < val) i++;
 	//fa l'interpolazione lineare:
@@ -32,10 +32,10 @@ double interp1(std::vector<T> x, std::vector<T> y, T val)
 
 
 
-
-std::vector<double> interp1(std::vector<double> x, std::vector<double> y, std::vector<double> vals)
+template<typename T>
+std::vector<T> interp1(std::vector<T> x, std::vector<T> y, std::vector<T> vals)
 {
-	std::vector<double> res;
+	std::vector<T> res;
 	
 	for(int i=0;i<vals.size();i++) res.push_back(interp1(x,y, vals[i]));
 	
