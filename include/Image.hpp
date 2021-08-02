6f35babdd04d2c28d3722e3c5477d23a5c6c0b5e
mrc-tech <diescc@gmail.com>
2021-08-02 02:22:57 +0200

    draw geometry
    methods for primitive geometry drawing in image-space coordinates

--- include/Image.hpp
+++ include/Image.hpp
@@ -32,10 +32,13 @@ class Image
 		uint H; // height
 		std::vector<color> data;
 		
+		unsigned _penWidth;
+		color _penColor;
+		
 	public:
-		Image() : W(0), H(0) {}
-		Image(const uint width, const uint height) : W(width), H(height) { data.resize(W*H); }
-		Image(const Image& image) : W(image.W), H(image.H) { data.resize(W*H); }
+		Image() : W(0), H(0) { _penWidth = 1; _penColor = {0,0,0}; }
+		Image(const uint width, const uint height) : W(width), H(height) { data.resize(W*H); _penWidth = 1; _penColor = {0,0,0}; }
+		Image(const Image& image) : W(image.W), H(image.H) { data.resize(W*H); _penWidth = 1; _penColor = {0,0,0}; }
 		
 		//getters:
 		inline uint width()  const { return W; }
@@ -50,6 +53,13 @@ class Image
 		inline bool operator ! () { return (data.size() == 0) || (W == 0) || (H == 0); } //check if there is some data
 		inline void clear(const color value = {0x00,0x00,0x00}) { std::fill(data.begin(), data.end(), value); } //clear the image with byte v
 		
+		//drawers:
+		void drawPoint(int x,int y); //draw a point with internal penWidth and penColor
+		void line(int x0,int y0,int x1,int y1); //draw a line
+		void rect(int x0,int y0,int x1,int y1); //draw a rectangle
+		void circ(int centerx,int centery,int radius); //draw a circle
+		inline void penColor(const unsigned char& R,const unsigned char& G,const unsigned char& B) { _penColor = {R,G,B}; }
+		inline void penWidth(const unsigned int& width) { _penWidth = width; }
 		
 		//saveFile:
 		void save_bmp(const std::string&) const;
@@ -87,7 +97,95 @@ void Image::save_bmp(const std::string& fileName) const
 
 
 
+void Image::drawPoint(int x, int y)
+{
+	if( (x < 0) || (y < 0) || (x >= static_cast<int>(W)) || (y >= static_cast<int>(H)) ) return;
+	switch(_penWidth)
+	{
+		case 1: set_pixel(x,y, _penColor);
+				break;
+		case 2: {
+				set_pixel(x  ,y  , _penColor);
+				set_pixel(x+1,y  , _penColor);
+				set_pixel(x+1,y+1, _penColor);
+				set_pixel(x  ,y+1, _penColor);
+				}
+                break;
+		case 3: {
+				set_pixel(x  ,y-1, _penColor);
+				set_pixel(x-1,y-1, _penColor);
+				set_pixel(x+1,y-1, _penColor);
+				
+				set_pixel(x  ,y  , _penColor);
+				set_pixel(x-1,y  , _penColor);
+				set_pixel(x+1,y  , _penColor);
+				
+				set_pixel(x  ,y+1, _penColor);
+				set_pixel(x-1,y+1, _penColor);
+				set_pixel(x+1,y+1, _penColor);
+				}
+				break;
+		default: set_pixel(x,y, _penColor); break;
+	}
+}
 
+
+
+void Image::line(int x0, int y0, int x1, int y1)
+{
+	bool steep = false;
+	int sx = ((x1 - x0) > 0) ? 1 : -1;
+	int sy = ((y1 - y0) > 0) ? 1 : -1;
+	int dx = abs(x1 - x0);
+	int dy = abs(y1 - y0);
+	
+	if (dy > dx){
+		std::swap(x0,y0);
+		std::swap(dx,dy);
+		std::swap(sx,sy);
+		steep = true;
+	}
+	int e = 2 * dy - dx;
+	
+	for(int i=0; i<dx; ++i){
+		if(steep) drawPoint(y0,x0);
+		else      drawPoint(x0,y0);
+		
+		while(e >= 0){
+			y0 += sy;
+			e -= (dx << 1);
+		}
+		
+		x0 += sx;
+		e  += (dy << 1);
+	}
+	
+	drawPoint(x1,y1);
+}
+
+void Image::rect(int x0,int y0,int x1,int y1)
+{
+	line(x0,y0,x1,y0);
+	line(x0,y0,x0,y1);
+	line(x1,y1,x1,y0);
+	line(x1,y1,x0,y1);
+}
+
+void Image::circ(int centerx, int centery, int radius)
+{
+	int x = 0;
+	int d = (1 - radius) << 1;
+	
+	while(radius >= 0){
+		drawPoint(centerx + x, centery + radius);
+		drawPoint(centerx + x, centery - radius);
+		drawPoint(centerx - x, centery + radius);
+		drawPoint(centerx - x, centery - radius);
+		
+		if((d + radius) > 0) d -= ((--radius) << 1) - 1;
+		if(x > d) d += ((++x) << 1) + 1;
+	}
+}
 
 
 
---
+++ tests/draw_test.cpp
@@ -0,0 +1,23 @@
+#include "../include/Image.hpp"
+
+using namespace std;
+
+
+
+int main()
+{
+	Image img(100,100); //create image 100x100 pixels
+	img.clear({0,0,0}); //set all pixels black
+	img.penColor(255,255,255); //set pen color to WHITE
+	img.line(10,10,90,90);
+	img.rect(10,10,90,90);
+	img.circ(50,50,45);
+	img.penColor(255,0,0); //set pen color to RED
+	img.penWidth(3); //set pen width to 3 pixels
+	img.line(40,50,60,50);
+	img.line(50,40,50,60);
+	
+	img.save_bmp("draw_test.bmp");
+	
+	return 0;
+}
