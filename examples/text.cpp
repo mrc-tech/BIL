4e1026422cd6a3165290dd2a24b6cf2f9aa1919f
mrc-tech <diescc@gmail.com>
2021-08-06 14:51:28 +0200

    updated text example
    now prints only the 96 printable characters

--- examples/text.cpp
+++ examples/text.cpp
@@ -11,12 +11,12 @@ int main()
 	int cW = ZX_Spectrum_16x16[0][0]; // font width
 	int cH = ZX_Spectrum_16x16[0][1]; // font height
 
-	Image img(16*cW,8*cH);
+	Image img(16*cW,(8-2)*cH);
 	
 	img.penColor(255,255,255);
-	for(int y=0; y<8; y++){
+	for(int y=0; y<(8-2); y++){
 		for(int x=0; x<16; x++){
-			std::string str = " "; str[0] = (char)(y*16+x);
+			std::string str = " "; str[0] = (char)((y+2)*16+x);
 			img.text(x*cW, y*cH, str, ZX_Spectrum_16x16);
 		}
 	}
