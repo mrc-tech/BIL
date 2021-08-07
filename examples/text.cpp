baede7fd22ed0bd893d823fb68e8763b2f79355b
mrc-tech <diescc@gmail.com>
2021-08-07 03:55:52 +0200

    text example
    improved the text example (a bit)

--- examples/text.cpp
+++ examples/text.cpp
@@ -3,13 +3,17 @@ Prints all 7-bits US-ASCII characters
 there are 96 printable ASCII chars
 ************************************/
 #include <string>
+#include <vector>
 
 #include "../include/Image.hpp"
 
 int main()
 {
-	int cW = ZX_Spectrum_16x16[0][0]; // font width
-	int cH = ZX_Spectrum_16x16[0][1]; // font height
+//	std::vector<std::string> font = ZX_Spectrum_16x16;
+	std::vector<std::string> font = IBM_EGA_8x14;
+	
+	int cW = font[0][0]; // font width
+	int cH = font[0][1]; // font height
 
 	Image img(16*cW,(8-2)*cH);
 	
@@ -17,7 +21,7 @@ int main()
 	for(int y=0; y<(8-2); y++){
 		for(int x=0; x<16; x++){
 			std::string str = " "; str[0] = (char)((y+2)*16+x);
-			img.text(x*cW, y*cH, str, ZX_Spectrum_16x16);
+			img.text(x*cW, y*cH, str, font);
 		}
 	}
 	img.save_bmp("text.bmp");
