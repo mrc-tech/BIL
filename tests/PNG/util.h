d3be4adf5677ce1fad87bfaf9938d238b4629844
mrc-tech <diescc@gmail.com>
2021-11-11 02:11:22 +0100

    prove sceme...
    lodepng comunque pare una buona risorsa...

---
+++ tests/PNG/util.h
@@ -0,0 +1,15 @@
+// Reverse bits to change endianness
+template <typename T>
+T reverse(T n, unsigned bits_num)
+{
+    T rv = 0;
+REVERSE_BITS:
+    for (int i = 0; i < bits_num; i++)
+    {
+#pragma HLS PIPELINE
+        rv <<= 1;
+        rv |= (n & 0x01);
+        n >>= 1;
+    }
+    return rv;
+}
